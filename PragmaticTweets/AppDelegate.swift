13b7024e73d13803fa16b2dcf344bb4fcc1fe0a0
Kevin Pfefferle <kevin@pfefferle.co>
2016-04-10 10:48:51 -0400

    10.1: Create dummy suggestions request to Twitter API

--- PragmaticTweets/AppDelegate.swift
+++ PragmaticTweets/AppDelegate.swift
@@ -31,6 +31,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        guard let url = NSURL(string: "https://api.twitter.com/1.1/users/suggestions.json") else {
+            return
+        }
+        sendTwitterRequest(url,
+          params: [ : ],
+          completion: { (data, urlResponse, error) -> Void in
+            do {
+                let jsonObject = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions([]))
+                NSLog("suggestions JSON: \(jsonObject)")
+            } catch {
+                NSLog("json error: \(error)")
+            }
+        })
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
