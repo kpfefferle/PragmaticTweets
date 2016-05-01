b2724d5da8181c1bd53ca657dfb575be14f0e08d
Kevin Pfefferle <kevin@pfefferle.co>
2016-05-01 17:23:57 -0400

    11.2: Lay out tweet details view

--- PragmaticTweets/TweetDetailViewController.swift
+++ PragmaticTweets/TweetDetailViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class TweetDetailViewController: UIViewController {
 
+    @IBOutlet weak var userImageButton: UIButton!
+    @IBOutlet weak var userRealNameLabel: UILabel!
+    @IBOutlet weak var userScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var tweetImageView: UIImageView!
+
     var tweetIdString: String? {
         didSet {
             reloadTweetDetails()
