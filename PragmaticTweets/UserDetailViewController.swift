94a05388c49ff983e1286f340b2e00687f488f68
Kevin Pfefferle <kevin@pfefferle.co>
2016-05-04 21:25:49 -0400

    11.3: Define outlets for User Detail VC

---
+++ PragmaticTweets/UserDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  UserDetailViewController.swift
+//  PragmaticTweets
+//
+//  Created by Kevin Pfefferle on 5/4/16.
+//  Copyright © 2016 Kevin Pfefferle. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userRealNameLabel: UILabel!
+    @IBOutlet weak var userScreenNameLabel: UILabel!
+    @IBOutlet weak var userLocationLabel: UILabel!
+    @IBOutlet weak var userDescriptionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
