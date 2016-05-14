d46da9c0bf8fcd420f8efe2017d164b1ae3cdd4e
Kevin Pfefferle <kevin@pfefferle.co>
2016-05-14 17:54:13 -0400

    12.3: Override Split VC size class to enable split view on wider phones

--- PragmaticTweets/SizeClassOverrideViewController.swift
+++ PragmaticTweets/SizeClassOverrideViewController.swift
@@ -13,8 +13,18 @@ class SizeClassOverrideViewController: UIViewController {
     var embeddedSplitVC: UISplitViewController!
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "embedSplitViewSegue" {
+        if segue.identifier! == "embeddedSplitViewSegue" {
             embeddedSplitVC = segue.destinationViewController as! UISplitViewController
+        }
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize,
+      withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        if size.width > 480.0 {
+            let overrideTraits = UITraitCollection(horizontalSizeClass: .Regular)
+            setOverrideTraitCollection(overrideTraits, forChildViewController: embeddedSplitVC)
+        } else {
+            setOverrideTraitCollection(nil, forChildViewController: embeddedSplitVC)
         }
     }
     
